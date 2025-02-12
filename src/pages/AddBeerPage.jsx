48f95098631a0320199ab46073510874c797d9df
unknown <samuglez90@gmail.com>
2025-02-12 17:44:12 +0100

    Hecho Samuel

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers/new";
 function AddBeerPage() {
   // State variables to store the values of the form inputs. You can leave these as they are.
   const [name, setName] = useState("");
@@ -10,6 +12,7 @@ function AddBeerPage() {
   const [brewersTips, setBrewersTips] = useState("");
   const [attenuationLevel, setAttenuationLevel] = useState(0);
   const [contributedBy, setContributedBy] = useState("");
+  const navigate = useNavigate();
 
   // Handler functions for the form inputs. You can leave these as they are.
   const handleName = (e) => setName(e.target.value);
@@ -27,6 +30,30 @@ function AddBeerPage() {
   // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
   // 2. Use axios to make a POST request to the Beers API.
   // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newBeer = {
+      name,
+      tagline,
+      description,
+      image_url: imageUrl,
+      first_brewed: firstBrewed,
+      brewers_tips: brewersTips,
+      attenuation_level: attenuationLevel,
+      contributed_by: contributedBy,
+    };
+
+    try {
+      // Send a POST request to create a new beer
+      await axios.post(API_URL, newBeer);
+      
+      // Redirect to the list of all beers (assuming you have a route for it)
+      navigate("/");
+    } catch (error) {
+      console.error("Error adding beer:", error);
+    }
+  };
 
 
 
@@ -34,7 +61,7 @@ function AddBeerPage() {
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Name</label>
           <input
             className="form-control mb-4"
--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,13 +1,34 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "../components/Search";
-import beersJSON from "./../assets/beers.json";
+import { use } from "react";
+import axios from "axios";
+// import beersJSON from "./../assets/beers.json";
+
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers";
 
 
 
 function AllBeersPage() {
   // Mock initial state, to be replaced by data from the API. Once you retrieve the list of beers from the Beers API store it in this state variable.
-  const [beers, setBeers] = useState(beersJSON);
+  const [beers, setBeers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  useEffect(() => {
+    const fetchBeers = async () => {
+      try {
+        const url = searchQuery
+          ? `${API_URL}/search?q=${searchQuery}`
+          : API_URL;
+
+        const response = await axios.get(url);
+        setBeers(response.data);
+      } catch (error) {
+        console.error("Error fetching beers:", error);
+      }
+    };
+
+    fetchBeers();
+  }, [searchQuery]);
 
 
 
@@ -16,12 +37,24 @@ function AllBeersPage() {
   // 2. Use axios to make a HTTP request.
   // 3. Use the response data from the Beers API to update the state variable.
 
-
+  useEffect(() => {
+    axios
+      .get(API_URL)
+      .then((response) => {
+        setBeers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
   // The logic and the structure for the page showing the list of beers. You can leave this as it is for now.
   return (
     <>
-      <Search />
+      <Search value={searchQuery} onChange={handleSearchChange} />
 
       <div className="d-inline-flex flex-wrap justify-content-center align-items-center w-100 p-4">
         {beers &&
